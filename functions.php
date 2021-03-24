c72ec2d33ed7a0b9b71e92d5f4491e3a24cd4aa0
TheMobileProf <themobileprof.com@gmail.com>
2021-03-24 02:13:08 +0100

    reset all

--- functions.php
+++ functions.php
@@ -40,17 +40,17 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 
 
 remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
+//remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 5);
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
+//remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 
 
 function woocommerce_template_product_description() {
   woocommerce_get_template( 'single-product/tabs/description.php' );
 }
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_description', 20 );
+//add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_description', 20 );
 
-remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
+//remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 ?>
