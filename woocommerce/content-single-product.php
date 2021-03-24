d9e04036bc07abd78b1bf192a7d25c99b2c17392
TheMobileProf <themobileprof.com@gmail.com>
2021-03-24 01:18:44 +0100

    woocommerce customization

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -11,22 +11,24 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 3.4.0
+ * @package WooCommerce\Templates
+ * @version 3.6.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
+global $product;
+
 /**
  * Hook: woocommerce_before_single_product.
  *
- * @hooked wc_print_notices - 10
+ * @hooked woocommerce_output_all_notices - 10
  */
 do_action( 'woocommerce_before_single_product' );
 
 if ( post_password_required() ) {
-  echo get_the_password_form(); // WPCS: XSS ok.
-  return;
+	echo get_the_password_form(); // WPCS: XSS ok.
+	return;
 }
 ?>
 <header>
@@ -58,7 +60,7 @@ if ( post_password_required() ) {
 
 <main>
 
-    <div id="product-<?php the_ID(); ?>" <?php wc_product_class(); ?>>
+	<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
         <div class="container dark-grey-text">
 
@@ -67,18 +69,16 @@ if ( post_password_required() ) {
 
                 <!--Grid column-->
                 <div class="col-md-6 mb-4">
-
-        <?php
-          /**
-           * Hook: woocommerce_before_single_product_summary.
-           *
-           * @hooked woocommerce_show_product_sale_flash - 10
-           * @hooked woocommerce_show_product_images - 20
-           */
-          do_action( 'woocommerce_before_single_product_summary' );
-        ?>
-
-                </div>
+					<?php
+					/**
+					 * Hook: woocommerce_before_single_product_summary.
+					 *
+					 * @hooked woocommerce_show_product_sale_flash - 10
+					 * @hooked woocommerce_show_product_images - 20
+					 */
+					do_action( 'woocommerce_before_single_product_summary' );
+					?>
+				</div>
                 <!--Grid column-->
 
                 <!--Grid column-->
@@ -86,25 +86,24 @@ if ( post_password_required() ) {
                      <!--Content-->
                     <div class="p-4">
 
-                    <div class="summary entry-summary">
-            <?php
-              /**
-               * Hook: woocommerce_single_product_summary.
-               *
-               * @hooked woocommerce_template_single_title - 5
-               * @hooked woocommerce_template_single_rating - 10
-               * @hooked woocommerce_template_single_price - 10
-               * @hooked woocommerce_template_single_excerpt - 20
-               * @hooked woocommerce_template_single_add_to_cart - 30
-               * @hooked woocommerce_template_single_meta - 40
-               * @hooked woocommerce_template_single_sharing - 50
-               * @hooked WC_Structured_Data::generate_product_data() - 60
-               */
-              do_action( 'woocommerce_single_product_summary' );
-            ?>
-          </div>
-
-                    </div>
+						<div class="summary entry-summary">
+							<?php
+							/**
+							 * Hook: woocommerce_single_product_summary.
+							 *
+							 * @hooked woocommerce_template_single_title - 5
+							 * @hooked woocommerce_template_single_rating - 10
+							 * @hooked woocommerce_template_single_price - 10
+							 * @hooked woocommerce_template_single_excerpt - 20
+							 * @hooked woocommerce_template_single_add_to_cart - 30
+							 * @hooked woocommerce_template_single_meta - 40
+							 * @hooked woocommerce_template_single_sharing - 50
+							 * @hooked WC_Structured_Data::generate_product_data() - 60
+							 */
+							do_action( 'woocommerce_single_product_summary' );
+							?>
+						</div>
+					</div>
                     <!--Content-->
 
                 </div>
@@ -113,46 +112,47 @@ if ( post_password_required() ) {
             </div>
             <!--Grid row-->
 
-            <hr>
+			<hr>
 
-            <?php if (wc_get_related_products(get_the_ID())) {  ?>
+			<?php if (wc_get_related_products(get_the_ID())) {  ?>
 
-            <!--Grid row-->
-            <div class="row d-flex justify-content-center wow fadeIn">
+			<!--Grid row-->
+			<div class="row d-flex justify-content-center wow fadeIn">
 
-                <!--Grid column-->
-                <div class="col-md-6 text-center">
-                    <h4 class="my-4 h4">Related products</h4>
-                </div>
-                <!--Grid column-->
+				<!--Grid column-->
+				<div class="col-md-6 text-center">
+					<h4 class="my-4 h4">Related products</h4>
+				</div>
+				<!--Grid column-->
 
-            </div>
-            <!--Grid row-->
+			</div>
+			<!--Grid row-->
 
-            <!--Grid row-->
-            <div class="row wow fadeIn">
+			<!--Grid row-->
+			<div class="row wow fadeIn">
 
-      <?php
-        /**
-         * Hook: woocommerce_after_single_product_summary.
-         *
-         * @hooked woocommerce_output_product_data_tabs - 10
-         * @hooked woocommerce_upsell_display - 15
-         * @hooked woocommerce_output_related_products - 20
-         */
-        do_action( 'woocommerce_after_single_product_summary' );
-      ?>
+				<?php
+				/**
+				 * Hook: woocommerce_after_single_product_summary.
+				 *
+				 * @hooked woocommerce_output_product_data_tabs - 10
+				 * @hooked woocommerce_upsell_display - 15
+				 * @hooked woocommerce_output_related_products - 20
+				 */
+				do_action( 'woocommerce_after_single_product_summary' );
+				?>
 
-            </div>
-            <!--Grid row-->
-            <?php } ?>
+			</div>
+			<!--Grid row-->
+			<?php } ?>
 
-    </div>
-          <!--Container  -->
+		</div>
+        <!--Container  -->
 
-  </div>
+  	</div>
     <!--Div product -->
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+
+	<?php do_action( 'woocommerce_after_single_product' ); ?>
 
 </main>
