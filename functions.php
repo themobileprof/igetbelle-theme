6594cd8375312562ecc41c9456bbaa51d68371f8
TheMobileProf <themobileprof.com@gmail.com>
2021-03-27 00:05:51 +0100

    add cart to menu

--- functions.php
+++ functions.php
@@ -47,18 +47,77 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
 
 
 remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10);
-//remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
+
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 5);
-//remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
 
 
-function woocommerce_template_product_description() {
-  woocommerce_get_template( 'single-product/tabs/description.php' );
+
+add_shortcode ('woo_cart_but', 'woo_cart_but' );
+/**
+ * Create Shortcode for WooCommerce Cart Menu Item
+ */
+function woo_cart_but() {
+	ob_start();
+ 
+        $cart_count = WC()->cart->cart_contents_count; // Set variable for cart item count
+        $cart_url = wc_get_cart_url();  // Set Cart URL
+  
+        ?>
+        <li><a class="menu-item cart-contents" href="<?php echo $cart_url; ?>" title="My Basket">
+	    <?php
+        if ( $cart_count > 0 ) {
+       ?>
+            <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php
+        }
+        ?>
+        </a></li>
+        <?php
+	        
+    return ob_get_clean();
+ 
 }
-//add_action( 'woocommerce_single_product_summary', 'woocommerce_template_product_description', 20 );
 
 
-//remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
-?>
+
+add_filter( 'woocommerce_add_to_cart_fragments', 'woo_cart_but_count' );
+/**
+ * Add AJAX Shortcode when cart contents update
+ */
+function woo_cart_but_count( $fragments ) {
+ 
+    ob_start();
+    
+    $cart_count = WC()->cart->cart_contents_count;
+    $cart_url = wc_get_cart_url();
+    
+    ?>
+    <a class="cart-contents menu-item" href="<?php echo $cart_url; ?>" title="<?php _e( 'View your shopping cart' ); ?>">
+	<?php
+    if ( $cart_count > 0 ) {
+        ?>
+        <span class="cart-contents-count"><?php echo $cart_count; ?></span>
+        <?php            
+    }
+        ?></a>
+    <?php
+ 
+    $fragments['a.cart-contents'] = ob_get_clean();
+     
+    return $fragments;
+}
+
+
+
+add_filter( 'wp_nav_menu_top-menu_items', 'woo_cart_but_icon', 10, 2 ); // Change menu to suit - example uses 'top-menu'
+
+/**
+ * Add WooCommerce Cart Menu Item Shortcode to particular menu
+ */
+function woo_cart_but_icon ( $items, $args ) {
+       $items .=  '[woo_cart_but]'; // Adding the created Icon via the shortcode already created
+       
+       return $items;
+}
